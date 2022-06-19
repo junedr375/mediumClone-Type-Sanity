f86a87d9264984cc880ad333ed6ecd48a7d465b3
Juned Raza <49837673+junedr375@users.noreply.github.com>
2022-06-19 22:19:47 +0530

    added package

--- pages/api/createComment.ts
+++ pages/api/createComment.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import  sanityClient from '@sanity/client'
+import sanityClient from '@sanity/client'
 
 const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
